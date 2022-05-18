687e062840ba02b0729f208fd0f28097a287fbeb
Sergio <sergio@localhost.localdomain>
2022-05-18 15:01:27 -0700

    Generate the figure in different formats and played around with x and yticks

--- Bin/eDAR_Figure.py
+++ Bin/eDAR_Figure.py
@@ -22,7 +22,6 @@ import sys
 
 #1. MAIN
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 #eDAR as a vector
 Min_edar=0;Max_edar=1e6
 Resolution=100;Steps=Resolution*Max_edar + 1;
@@ -36,12 +35,16 @@ Temperateness=edar**exponent/(edar**exponent+K_edar)
 
 #PLOTS
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Path to save figures
+Output_Path='/home/sergio/work/Github/LysogenyModel/Results/Figures/'
+#Extensions
+Extensions=['.pdf','.eps','.png','.svg']
 
 #Fontsizes
 size_axis=7;size_ticks=5;size_title=5
 #Figure Size
 cm = 1/2.54  # centimeters in inches
-Width=8*cm;Height=4*cm #Width and height of plots
+Width=8*cm;Height=7*cm #Width and height of plots
 figure(figsize=(Width, Height), dpi=300)
 
 #Colors
@@ -55,8 +58,16 @@ print(Temperateness)
 #Axes, title, and ticks
 plt.ylabel('Temperateness', fontsize=size_axis)
 plt.xlabel('e-DAR (J/g)', fontsize=size_axis)
+plt.ylim([0,1])
+yticks_labels=[0, 0.5, 1]
+plt.yticks(yticks_labels, fontsize=size_ticks)
+
+xticks_labels=[Min_edar, 0.5*(Min_edar + Max_edar), Max_edar]
+plt.xticks(xticks_labels, fontsize=size_ticks)
+#plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 
 sns.despine(top=True, right=True, left=False, bottom=False) 
 
-
+Name_Fig='Figure_2'
+[plt.savefig(Output_Path+Name_Fig+ext,dpi=300) for ext in Extensions]
 plt.show()
